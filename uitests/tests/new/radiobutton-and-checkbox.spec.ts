9f2f64aa4a16dc24566db7f4629eefa5b121cbdc
XIN-CHI Lo <ashley.lo@scrive.com>
2022-06-03 11:44:04 +0200

    add assertion

--- uitests/tests/new/radiobutton-and-checkbox.spec.ts
+++ uitests/tests/new/radiobutton-and-checkbox.spec.ts
@@ -1,8 +1,12 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, Locator } from "@playwright/test";
 
 test("test checkboxes and Radio buttons without placements", async ({ page }) => {
   const newSignUrl = "https://staging.scrive.com/s/9221714692412077704/9221932570715111975/e2d6e750aba8283a";
 
-  await page.goto(newSignUrl, { waitUntil: "networkidle" });
+  await page.goto(newSignUrl);
   await expect(page).toHaveURL(/new\/sign/);
+
+  const checkbox: Locator = page.locator("input[type=checkbox]");
+  // await checkbox.waitFor();
+  await expect(checkbox).toBeVisible();
 });
